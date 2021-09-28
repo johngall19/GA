d65b52dd77842a85a38a64cb165dc1b2812fe735
Andrew Beattie <andrew.beattie@liberty-it.co.uk>
2021-09-28 11:44:24 +0100

    fixed mutation bugs

--- mutations.py
+++ mutations.py
@@ -1,9 +1,11 @@
 import random
 
+child = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
 
 def mutation_swap(child):
-    cell1 = random.randint(0, len(child) - 1)
-    cell2 = random.randint(0, len(child) - 1)
+    cell1 = random.randint(1, len(child) - 2)
+    cell2 = random.randint(1, len(child) - 2)
     if cell1 == cell2:
         mutation_swap(child)
     child[cell1], child[cell2] = child[cell2], child[cell1]
@@ -14,21 +16,22 @@ def mutation_scramble(child):
     subset = []
     subsetSize = int(len(child) / 2)
     for _ in range(subsetSize):
-        subset.append(child[_])
+        subset.append(child[_ + 1])
     random.shuffle(subset)
     for _ in range(subsetSize):
-        child[_] = subset[_]
+        child[_ + 1] = subset[_]
+
     return child
 
 
-def mutated_invert(child):
-    subsetSize = random.randint(int(len(child) / 3), int(len(child)))
+def mutation_invert(child):
+    subsetSize = random.randint(int(len(child) / 3), int(len(child) - 2))
     subset = []
     for _ in range(subsetSize):
-        subset.append(child[_])
+        subset.append(child[_ + 1])
     subset.reverse()
     for _ in range(subsetSize):
-        child[_] = subset[_]
+        child[_ + 1] = subset[_]
     return child
 
 
